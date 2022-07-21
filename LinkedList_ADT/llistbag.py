af6752abf97c168b58daf87aa0a85654210a70bd
Mofiro Jean <mofirojean@gmail.com>
2022-07-21 22:17:36 +0100

    added an iterator class

--- LinkedList_ADT/llistbag.py
+++ LinkedList_ADT/llistbag.py
@@ -43,9 +43,31 @@ class Bag:
         else:
             print(f"{item} not found in the bag")
 
+    # Returns and iterator for traversing the list of items
+    def __iter__(self):
+        return _LinkedListIterator(self._head)
+
 
 # Defines a private storage class for creating list nodes
 class _BagListNode:
     def __init__(self, item):
         self._item = item
         self.next = None
+
+
+# Defines a linked list iterator
+class _LinkedListIterator:
+    def __init__(self, listHead):
+        self._curNode = listHead
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self._curNode is None:
+            raise StopIteration
+        else:
+            item = self._curNode.next
+            self._curNode = self._curNode.next
+            return item
+
