a5a8cd44e0cf1735499186da554b4ad9c01ec192
Mofiro Jean <mofirojean@gmail.com>
2022-07-21 22:01:41 +0100

    added more methods for the bagll

--- LinkedList_ADT/llistbag.py
+++ LinkedList_ADT/llistbag.py
@@ -24,6 +24,25 @@ class Bag:
         self._head = newNode
         self._size += 1
 
+    # Removes an instance of the item from the bag
+    def remove(self, item):
+        predNode = None
+        curNode = self._head
+        while curNode is not None and curNode.data != item:
+            predNode = curNode
+            curNode = curNode.next
+
+        # The item has to be in the bag to remove it
+        if curNode is not None:
+            self._size -= 1
+            if curNode is self._head:
+                self._head = curNode.next
+            else:
+                predNode.next = curNode.next
+            return curNode.item
+        else:
+            print(f"{item} not found in the bag")
+
 
 # Defines a private storage class for creating list nodes
 class _BagListNode:
