529368f26da4e13409fcf6f65001b3104b49c7fd
Mofiro Jean <mofirojean@gmail.com>
2022-07-25 21:26:10 +0100

    completed the isValidSource method

--- Stack_ADT/delimiter_balance.py
+++ Stack_ADT/delimiter_balance.py
@@ -3,4 +3,21 @@
 from stack_linked_list import Stack
 
 
+def isValidSource(srcfile):
+    s = Stack()
+    for line in srcfile:
+        for char in line:
+            if char in "{[(":
+                s.push(char)
+            elif char in "}])":
+                if s.isEmpty():
+                    return False
+                else:
+                    left = s.pop()
+                    if (char == "}" and left != "{") or \
+                            (char == "]" and left != "[") or \
+                            (char == ")" and left != "("):
+                        return False
+    return s.isEmpty()
 
+# Driver method
