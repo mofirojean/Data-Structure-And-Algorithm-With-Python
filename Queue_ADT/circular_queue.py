2f2be8bb72e3f020a5a43f9ee01a0518984864bd
Mofiro Jean <mofirojean@gmail.com>
2022-07-26 23:36:04 +0100

    added the enqueue and dequeue method of the circular queue ADT

--- Queue_ADT/circular_queue.py
+++ Queue_ADT/circular_queue.py
@@ -18,3 +18,37 @@ class Queue:
     def isFull(self):
         return self._count == len(self._qArray)
 
+    # Returns the number of items in the queue.
+    def __len__(self):
+        return self._count
+
+    # Adds the given item to the queue
+    def enqueue(self, item):
+        if not self.isFull():
+            maxSize = len(self._qArray)
+            self._back = (self._back + 1) % maxSize
+            self._qArray[self._back] = item
+            self._count += 1
+        else:
+            print("Cannot enqueue to a full queue")
+
+    # Removes and returns the first item in the queue
+    def dequeue(self):
+        if not self.isEmpty():
+            item = self._qArray[self._front]
+            maxSize = len(self._qArray)
+            self._front = (self._front + 1) % maxSize
+            self._count -= 1
+            return item
+        else:
+            print("Cannot dequeue from an empty queue")
+
+
+# Driver method
+if __name__ == "__main__":
+    myQ = Queue(5)
+    myQ.enqueue("Mary")
+    myQ.enqueue("John")
+    myQ.enqueue("Jean")
+    myQ.enqueue("Peter")
+    print(myQ)
